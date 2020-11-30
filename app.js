46298731378aaec653aa3ba61cf03184f47798c4
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-11-30 22:18:12 +0700

    Add themeplate

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var hbs = require('hbs');
+hbs.registerPartials(__dirname, 'views/partials');
+
 var app = express();
 
 // view engine setup
