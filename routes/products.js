f2803a287d703f7efccbc787bdd30733307ae86b
HUYNH HO THANH TRA <hothanhtrahuynh@github.com>
2020-12-01 08:46:19 +0700

    Routing

--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Dashboard' });
+    res.render('products', { title: 'Product' });
 });
 
-router.get('/login', function(req, res, next) {
-    res.render('login', { title: "Admin Area | Account Login" });
-});
 module.exports = router;
