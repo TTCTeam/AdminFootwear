dafae517449ae92d652a2d7d6777ed6f0944010b
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-12-07 10:43:19 +0700

    add accounts page and paging accounts page

--- models/accountModel.js
+++ models/accountModel.js
@@ -1,3 +1,24 @@
-exports.list = () => {
+const { ObjectID } = require('mongodb');
+const { db } = require('../dal/db');
+var assert = require('assert')
 
+exports.list = async() => {
+    const accountCollection = db().collection('Account');
+    const account = await accountCollection.find({}).toArray();
+    return account;
+}
+
+exports.paging = async(filter, pageNumber, nPerPage) => {
+
+    const account = await db().collection('Account').find(filter)
+        .limit(nPerPage)
+        .skip(pageNumber > 0 ? ((pageNumber - 1) * nPerPage) : 0)
+        .toArray();
+
+    return account;
+}
+
+exports.count = async(filter) => {
+    const accountCollection = db().collection('Account');
+    return await accountCollection.find(filter).count();
 }
